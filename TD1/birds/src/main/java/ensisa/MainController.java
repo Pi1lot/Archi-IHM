3d941885280ca9222611ae0dbe45ca944ba9cd58
Pi1lot <poellen.dorian@gmail.com>
2025-11-19 11:28:39 +0100

    Added birds name display

--- TD1/birds/src/main/java/ensisa/MainController.java
+++ TD1/birds/src/main/java/ensisa/MainController.java
@@ -1,13 +1,31 @@
 package ensisa;
 
 import ensisa.model.BirdRepository;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import ensisa.model.Bird;
+
 
 public class MainController {
 
     private BirdRepository repository;
 
+    @FXML
+    private Label commonNameLabel;
+    @FXML
+    private Label latinNameLabel;
+
+    public Bird currentBird;
+
     public MainController(){
         repository = new BirdRepository();
         repository.load();
+
+        currentBird = repository.birds.get(0);
+    }
+
+    public void initialize() {
+        commonNameLabel.setText(currentBird.getCommonName());
+        latinNameLabel.setText(currentBird.getLatinName());
     }
 }
