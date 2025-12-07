9da174e92e68a7ee0030a38edae522f8a48b288c
Pi1lot <poellen.dorian@gmail.com>
2025-12-07 13:13:29 +0100

    Added correct bird list

--- TD1/birds/src/main/java/ensisa/model/BirdCellFactory.java
+++ TD1/birds/src/main/java/ensisa/model/BirdCellFactory.java
@@ -1,6 +1,10 @@
 package ensisa.model;
-import javafx.scene.control.ListView;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 
 public class BirdCellFactory implements Callback<ListView<Bird>, ListCell<Bird>> {
@@ -8,17 +12,36 @@ public class BirdCellFactory implements Callback<ListView<Bird>, ListCell<Bird>>
     @Override
     public ListCell<Bird> call(ListView<Bird> listView) {
         return new ListCell<>() {
+            private VBox vbox;
+            private Label commonNameLabel;
+            private Label latinNameLabel;
+            private FXMLLoader loader;
+
             @Override
             protected void updateItem(Bird bird, boolean empty) {
                 super.updateItem(bird, empty);
+
                 if (empty || bird == null) {
                     setText(null);
                     setGraphic(null);
                 } else {
-                    setText(bird.getCommonName());
+                    if (loader == null) {
+                        loader = new FXMLLoader(getClass().getResource("/ensisa/bird-cell.fxml"));
+                        try {
+                            vbox = loader.load();
+                            commonNameLabel = (Label) vbox.lookup("#commonNameLabel");
+                            latinNameLabel = (Label) vbox.lookup("#latinNameLabel");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    commonNameLabel.setText(bird.getCommonName());
+                    latinNameLabel.setText(bird.getLatinName());
+                    setText(null);
+                    setGraphic(vbox);
                 }
             }
         };
     }
-
 }
--- TD1/birds/src/main/java/module-info.java
+++ TD1/birds/src/main/java/module-info.java
@@ -6,4 +6,7 @@ requires com.fasterxml.jackson.databind;
 opens ensisa to javafx.fxml, com.fasterxml.jackson.databind;
 exports ensisa;
 exports ensisa.model;
+
+opens ensisa.model to javafx.fxml;  // pour BirdCell et autres controllers dans model
+
 }
